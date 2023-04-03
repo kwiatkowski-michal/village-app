3e7999d794f7ac28913cdfe1075c03a88860b7f3
Michał Kwiatkowski <kwiatkowskimichal04@gmail.com>
2023-04-03 19:52:11 +0200

    Updates

---
+++ static/js/841.07faf973.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.07faf973.chunk.js.LICENSE.txt */
+"use strict";(globalThis.webpackChunkvillage_app=globalThis.webpackChunkvillage_app||[]).push([[841],{841:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>r});var s=n(554),a=n(743),o=n(811);const r=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,r=document.elementFromPoint(t/2,n/2);if(!r)return;const i=(0,a.a)(r);i&&new Promise((e=>(0,o.c)(i,e))).then((()=>{(0,s.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,a.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.07faf973.chunk.js.map
