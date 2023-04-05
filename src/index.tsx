9723ebbaa0731ba85e885226aee246d1665ee0a1
Michał Kwiatkowski <kwiatkowskimichal04@gmail.com>
2023-04-05 21:28:05 +0200

    clean install

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -11,4 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+
